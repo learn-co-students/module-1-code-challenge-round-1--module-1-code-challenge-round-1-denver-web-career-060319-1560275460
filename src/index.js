b5b1a999382ec01090737c776d646139b413354c
Jessica Pope <jessica.pope.co@gmail.com>
2019-06-11 13:41:13 -0600

    first round mod 3 code challenge

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 2809
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +9,39 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  const fetchSomething = async() =>{
+
+    try {
+      const response = await fetch(imageURL)
+      const result = await response.json()
+      console.log(result)
+    } catch(err) {
+      console.error(err)
+    }
+  }
+
+  fetchSomething()
+
+  const postSomething = async() =>{
+    const reqBody = {image_id: 2809}
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(reqBody),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+                }
+        })
+      } catch(err){
+
+        console.error(err)
+      }
+  }
+
 })
+
+const li = document.createElement("li")
+li.textContent = "I want my api info here"
+const ul = document.querySelector("#likes")
+ul.appendChild(li)
