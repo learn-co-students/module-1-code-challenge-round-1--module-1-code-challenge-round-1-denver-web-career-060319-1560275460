e3b40cd13c0cdb2777f3b39cbbcd4cfce2655fbc
Jessica Pope <jessica.pope.co@gmail.com>
2019-06-12 11:30:28 -0600

    this is a completely working version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+  // console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+  })
 
   let imageId = 2809
 
@@ -8,40 +9,107 @@ document.addEventListener('DOMContentLoaded', () => {
   const likeURL = `https://randopic.herokuapp.com/likes/`
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
+  const domImage = document.getElementById('image')
+  domImage.src = ""
+  const name = document.getElementById('name')
+  const likeCount = document.getElementById('likes')
+  const comments = document.getElementById('comments')
+  const likeButton = document.getElementById('like_button')
+  const submitButton = document.querySelectorAll('input')[1]
+  const textBox = document.getElementById('comment_input')
 
-  const fetchSomething = async() =>{
 
-    try {
-      const response = await fetch(imageURL)
-      const result = await response.json()
-      console.log(result)
-    } catch(err) {
-      console.error(err)
+function fetchImage(){
+  return fetch(imageURL)
+  .then(response => response.json())
+  .then(response => {
+    domImage.src = response.url
+    return response.id
+  })
+}
+
+function fetchName(){
+  return fetch(imageURL)
+  .then(response => response.json())
+  .then(response => {
+    name.innerText = response.name
+    return response.id
+  })
+}
+
+function fetchLikeCount(){
+  return fetch(imageURL)
+  .then(response => response.json())
+  .then(response => {
+    likeCount.innerText = response.like_count
+    return response.id
+  })
+}
+
+function fetchComments(){
+  return fetch(imageURL)
+  .then(response => response.json())
+  .then(response => {
+    response.comments.forEach(function(curent){
+      li = document.createElement('li')
+      li.textContent = curent.content
+      comments.appendChild(li)
     }
-  }
+    )
+    return response.id
+  })
+}
 
-  fetchSomething()
+likeButton.addEventListener("click", function(e){
+  let likes = parseInt(likeCount.innerText)
+  likeCount.innerText = likes + 1
+  let currentLikes = likeCount.innerText
+  const postLikes = async () =>{
+   const reqBody = {image_id: 2809, image_likes:currentLikes}
+   try {
+     const response = await fetch(likeURL, {
+       method: 'POST',
+       body: JSON.stringify(reqBody),
+       headers: {
+         'Accept': 'application/json',
+         'Content-Type': 'application/json'
+               }
+       })
+     } catch(err){
 
-  const postSomething = async() =>{
-    const reqBody = {image_id: 2809}
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(reqBody),
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-                }
-        })
-      } catch(err){
-
-        console.error(err)
-      }
-  }
-
+       console.error(err)
+     }
+ }
+postLikes()
 })
 
-const li = document.createElement("li")
-li.textContent = "I want my api info here"
-const ul = document.querySelector("#likes")
-ul.appendChild(li)
+submitButton.addEventListener("click", function(e){
+  e.preventDefault()
+let newComment = textBox.value
+li = document.createElement('li')
+li.textContent = newComment
+comments.appendChild(li)
+textBox.value = ""
+const postComments = async () =>{
+ const reqBody = {image_id: 2809, content:newComment}
+ try {
+   const response = await fetch(commentsURL, {
+     method: 'POST',
+     body: JSON.stringify(reqBody),
+     headers: {
+       'Accept': 'application/json',
+       'Content-Type': 'application/json'
+             }
+     })
+   } catch(err){
+
+     console.error(err)
+   }
+}
+postComments()
+})
+
+fetchComments()
+fetchLikeCount()
+fetchName()
+fetchImage()
